5b348727a34418c51aed5b202e5e8a430a9f0c64
Ismael Borges <ismaelbb26@gmail.com>
2020-09-21 10:53:32 -0300

    calculo de distancias quando os eventos ocorrem adicionados

--- js/slide.js
+++ js/slide.js
@@ -2,33 +2,49 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
   }
-  noInicio(event) {
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  quandoAtualizaPosicao(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.5;
+    return this.dist.finalPosition - this.dist.movement;
+  }
+
+  quandoInicia(event) {
     event.preventDefault();
-    this.wrapper.addEventListener('mousemove', this.quandoMover);
-    console.log('mousedown');
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener('mousemove', this.quandoMove);
   }
 
-  quandoMover(event) {
-    this.wrapper.removeEventListener('mousedown', this.quandoMover);
+  quandoMove(event) {
+    const finalPosition = this.quandoAtualizaPosicao(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
-  quandoTerminar(event) {}
+  quandoTermina(event) {
+    this.wrapper.removeEventListener('mousemove', this.quandoMove);
+    this.dist.finalPosition = this.dist.movePosition;
+  }
 
-  addEventosSlide() {
-    this.wrapper.addEventListener('mousedown', this.noInicio);
-    this.wrapper.addEventListener('mouseup', this.quandoTerminar);
+  addSlideEventos() {
+    this.wrapper.addEventListener('mousedown', this.quandoInicia);
+    this.wrapper.addEventListener('mouseup', this.quandoTermina);
   }
 
   bindEventos() {
-    this.noInicio = this.noInicio.bind(this);
-    this.quandoMover = this.quandoMover.bind(this);
-    this.quandoTerminar = this.quandoTerminar.bind(this);
+    this.quandoInicia = this.quandoInicia.bind(this);
+    this.quandoMove = this.quandoMove.bind(this);
+    this.quandoTermina = this.quandoTermina.bind(this);
   }
 
   init() {
     this.bindEventos();
-    this.addEventosSlide();
+    this.addSlideEventos();
     return this;
   }
 }
